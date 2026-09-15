e013bb1e5db4f350020f927daad7080d903fe415
Andrew Helsby <ajhelsby@hotmail.com>
2026-09-15 22:04:12 +0400

    feat(#22): Simplify JsonValue type alias to object for flexibility

--- app/domain/models/json.py
+++ app/domain/models/json.py
@@ -2,8 +2,4 @@
 
 from __future__ import annotations
 
-from typing import TypeAlias
-
-JsonValue: TypeAlias = (  # noqa: UP040
-    None | bool | int | float | str | list["JsonValue"] | dict[str, "JsonValue"]
-)
+JsonValue = object
